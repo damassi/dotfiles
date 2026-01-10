25ade7fd2b979904b91ceaa4d2dc69c959346154
Christopher Pappas <christopher.pappas@sentinelone.com>
2026-01-10 12:04:20 -0800

    Update nvim and tmux configs

--- dotfiles/nvim/bootstrap.lua
+++ dotfiles/nvim/bootstrap.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
@@ -6,6 +7,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "--branch=stable", lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(require("plugins"))
